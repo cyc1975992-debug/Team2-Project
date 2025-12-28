2f126fb0c8b95005b31b3dc8bbe16a5a31881ca6
ppisong <ppisong@gmail.com>
2025-12-28 21:27:11 +0900

    수집 분석(ppisong) 파일, 공격(01input), 시각화(01output) 파일

--- 01input.py
+++ 01input.py
@@ -1,6 +1,4 @@
-from scapy.all import sniff
-from scapy.all import Ether, IP, TCP, ICMP, send, sr1
-
+from scapy.all import IP, TCP, ICMP, send
 # ICMP 패킷 하나 생성하고 전송
 packet = IP(dst ="172.30.1.15") /ICMP()
 for _ in range(4):
@@ -12,11 +10,11 @@ for _ in range(4):
     send(packet)
 
 # sr1 = send + receive1 response
-packet = IP(dst="210.115.158.17") / ICMP()
-reply = sr1(packet, timeout=2, verbose=False)
-
-if reply:
-    reply.summary()
+# packet = IP(dst="210.115.158.17") / ICMP()
+# reply = sr1(packet, timeout=2, verbose=False)
+#
+# if reply:
+#     reply.summary()
 
 
 
