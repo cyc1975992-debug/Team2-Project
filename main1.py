6aa640d300435074ca769093802f6cdf8c6638b7
gwang min <dwc06003@gmail.com>
2025-12-31 09:51:46 +0900

    실행 페이지 추가 메인화면 추가

--- main1.py
+++ main1.py
@@ -11,10 +11,11 @@ if 'engine_on' not in st.session_state:
 
 # [내비게이션 설정] 첨부하신 13streamlit_multi_pages.py 스타일 적용
 pg = st.navigation([
-    st.Page("packet_log.py", title="1. 패킷 항목", icon="📡"),
-    st.Page("visual_charts.py", title="2. 그래프 항목", icon="📈"),
-    st.Page("csv_manager.py", title="3. CSV DB 생성", icon="💾"),
-    st.Page("security_ips.py", title="4. 방어 항목", icon="🛡️")
+    st.Page("project_main_page.py", title="Main", icon="🗼"),
+    st.Page("packet_log.py", title="패킷 항목", icon="📡"),
+    st.Page("visual_charts.py", title="그래프 항목", icon="📈"),
+    st.Page("csv_manager.py", title="CSV DB 생성", icon="💾"),
+    st.Page("security_ips.py", title="차단 목록", icon="🛡️")
 ])
 
 pg.run()
